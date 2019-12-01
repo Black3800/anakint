3459432615ca935291d094e00df9b8db8dce7727
Arnonym <black38000@gmail.com>
2019-12-02 00:56:57 +0700

    added build

---
+++ build/precache-manifest.45ad877ae098f1095e40f1f92fe286c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41cd38571bf573e25c00d5b540c1a68b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "487384a228c53440a0d4",
+    "url": "/static/css/main.459b5b02.chunk.css"
+  },
+  {
+    "revision": "a3446cb27b5ec0ab99a9",
+    "url": "/static/js/2.cb022dd5.chunk.js"
+  },
+  {
+    "revision": "487384a228c53440a0d4",
+    "url": "/static/js/main.90a1a876.chunk.js"
+  },
+  {
+    "revision": "8cd7635a3719be7b22f3",
+    "url": "/static/js/runtime-main.548d88d4.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
