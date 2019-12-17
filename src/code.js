b6a0dae665e03ea2a8e49c155fe37ed96adf3e2a
Arnonym <black38000@gmail.com>
2019-12-17 18:58:49 +0700

    added information for i_code

--- src/code.js
+++ src/code.js
@@ -7,9 +7,15 @@ class Code extends React.Component {
         return (
             <div className="code content-card">
                 <div>
-                    <Fade>
-                        <Heading text="i_code" />
-                    </Fade>
+                    <Heading text="i_code" />
+                </div>
+                <img srcSet="code@0,25x.jpg,
+                             code@0,5x.jpg 1.5x,
+                             code@0,75x.jpg 2x,
+                             code.jpg 2.5x" src="code.jpg" />
+                <div className="textbox">
+                    <h2>What made you enjoy coding?</h2>
+                    Mine is the feeling that I could build the whole world up with my own keystrokes, my codes, and my passion.
                 </div>
             </div>
         );
