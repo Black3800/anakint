7b32e4bb6dd44c341091894cb95b67bb4ed86201
Arnonym <black38000@gmail.com>
2019-12-17 17:51:05 +0700

    added information for about_me

--- src/about.js
+++ src/about.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Fade from 'react-reveal/Fade';
 import Heading from './heading.js';
 //import { Section } from 'react-fullpage';
 
@@ -10,13 +9,12 @@ class About extends React.Component
         return (
             <div className="about content-card dark">
                 <div>
-                    <Fade>
-                        <Heading text="about_me" dark="true" />
-                    </Fade>
+                    <Heading text="about_me" dark="true" />
                 </div>
-                <div class="dat">
-                    sadkdhsidhsad
-                </div>
+                <div class="center img"></div>
+                <p class="center">
+                    I am a student at Potisarnpittayakorn School. I started coding when I was 10. Simple HTML page I had built on that day sparked my interest in the computer field. Since that day, I have been continuously educating myself.
+                </p>
             </div>
         );
     }
